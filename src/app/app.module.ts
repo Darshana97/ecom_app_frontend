2178415d5fe3104186c69707c0438f7d49ce5493
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-10-12 22:31:44 +0530

    add home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { CartComponent } from './components/cart/cart.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { CheckoutComponent } from './components/checkout/checkout.component';
     HeaderComponent,
     FooterComponent,
     CartComponent,
-    CheckoutComponent
+    CheckoutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
