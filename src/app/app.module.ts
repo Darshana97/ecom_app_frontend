c063dbddb107f31f1869938be12ac2b0d7c284ef
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-10-14 19:00:45 +0530

    http client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -25,8 +27,8 @@ import { ProductService } from './services/product.service';
   ],
   imports: [
     BrowserModule,
-    
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
