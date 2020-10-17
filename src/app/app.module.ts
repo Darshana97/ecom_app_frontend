c096db074a088c829f5fcafaeaa0e72dbd38f6da
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-10-17 21:38:21 +0530

    ecom

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { CartComponent } from './components/cart/cart.component';
-import { CheckoutComponent } from './components/checkout/checkout.component';
-import { HomeComponent } from './components/home/home.component';
-import { ProductComponent } from './components/product/product.component';
-import { ThankyouComponent } from './components/thankyou/thankyou.component';
-import { ProductService } from './services/product.service';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { CartComponent } from "./components/cart/cart.component";
+import { CheckoutComponent } from "./components/checkout/checkout.component";
+import { HomeComponent } from "./components/home/home.component";
+import { ProductComponent } from "./components/product/product.component";
+import { ThankyouComponent } from "./components/thankyou/thankyou.component";
+import { ProductService } from "./services/product.service";
 
 @NgModule({
   declarations: [
@@ -23,14 +23,10 @@ import { ProductService } from './services/product.service';
     CheckoutComponent,
     HomeComponent,
     ProductComponent,
-    ThankyouComponent
+    ThankyouComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [ProductService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
