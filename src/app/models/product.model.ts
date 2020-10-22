a8e2f802a493f5084a55cde7919786a7e6dadfbb
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-10-22 18:56:00 +0530

    product model

---
+++ src/app/models/product.model.ts
@@ -0,0 +1,13 @@
+export interface ProductModelServer {
+  id: number;
+  name: string;
+  category: string;
+  description: string;
+  price: number;
+  quantity: number;
+}
+
+export interface ServerResponse {
+  count: number;
+  products: ProductModelServer[];
+}
