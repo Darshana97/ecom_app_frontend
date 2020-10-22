933cb8f0f2d1cb3c7eb127b20e7a2793840de340
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-10-22 19:25:33 +0530

    home

--- src/app/models/product.model.ts
+++ src/app/models/product.model.ts
@@ -4,7 +4,9 @@ export interface ProductModelServer {
   category: string;
   description: string;
   price: number;
+  image: string;
   quantity: number;
+  images: string;
 }
 
 export interface ServerResponse {
